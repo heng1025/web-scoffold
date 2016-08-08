f59de5199e3879eacbdc7dcb0413317bf1bd5278
Heng <Heng>
2016-08-08 11:11:09 +0800

    完善功能

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 'use strict';
 
-/* es6模块加载 */
+/* es6模块方式加载 main.scss */
 import './scss/main.scss';
+
+/* 加载main.js */
+import './js/main.js';
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+
+var btn = document.getElementById('btn');
+
+btn.onclick = function(){
+	alert('你点击了按钮');
+};
